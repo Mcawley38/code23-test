467f4d7e323ad978389423913c244de744e4bb69
Myles Cawley <myles.cawley@ultimate-uk.com>
2020-11-20 20:03:04 +0000

    - abandoned deck loader concept as promises were proving to be a nightmare and time consuming - Game component barebones created

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,14 +1,54 @@
 <template>
     <main>
+        <div>
+            <span>Score: {{score.current_score}}</span>
+            <span>Lives:  {{score.lives}}<span></span></span>
+        </div>
+        <div>
+            {{score.high_score}}
+        </div>
         <div class="w-56 h-56 bg-red-500"></div>
+        <a>High</a>
+        <a>low</a>
     </main>
 </template>
 
 <script>
+    import axios from 'axios'
+
     export default {
-        name: 'HelloWorld',
+        name: 'Game',
         data: () => ({
-           test:'test'
-        })
+            deck: [],
+            lives: 0,
+            score: {
+                high_score: 0,
+                current_score: 0,
+            }
+        }),
+        methods: {
+            getDeck() {
+                axios.get('https://higher-lower.code23.com/api/deck')
+                    .then(response => {
+                        this.deck = response.data
+                    })
+            },
+            shuffle() {
+                for (let i = this.deck.length - 1; i > 0; i--) {
+                    const j = Math.floor(Math.random() * (i + 1));
+                    [this.deck[i], this.deck[j]] = [this.deck[j], this.deck[i]]
+                }
+            },
+            resetGame() {
+                this.lives = 3;
+                this.score = 0;
+
+                this.shuffle();
+            },
+            takeTurn() {
+                const new_card = this.deck.splice(index, 1)[0]
+
+            }
+        }
     }
 </script>
