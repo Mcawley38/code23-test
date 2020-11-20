2602aede062fa96f4de34bac221dfbe24de7184c
Myles Cawley <myles.cawley@ultimate-uk.com>
2020-11-20 19:25:13 +0000

    - set up tailwindcss and converted HellowWorld component into game component

---
+++ src/components/Game.vue
@@ -0,0 +1,14 @@
+<template>
+    <main>
+        <div class="w-56 h-56 bg-red-500"></div>
+    </main>
+</template>
+
+<script>
+    export default {
+        name: 'HelloWorld',
+        data: () => ({
+           test:'test'
+        })
+    }
+</script>
